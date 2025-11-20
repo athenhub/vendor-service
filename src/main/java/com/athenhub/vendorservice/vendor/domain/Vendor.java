456baf9728f6df2bf244e2a3ea967e8066158fcb
devangelwings <khs96523@gmail.com>
2025-11-20 11:53:56 +0900

    REFACTOR: 파라미터 명을 용도를 명확히 표현하도록 변경
    See also: #1

--- src/main/java/com/athenhub/vendorservice/vendor/domain/Vendor.java
+++ src/main/java/com/athenhub/vendorservice/vendor/domain/Vendor.java
@@ -91,19 +91,19 @@ public class Vendor extends AbstractAuditEntity {
    *
    * <p>필수 값은 {@code request} 내부에서 유효성 검증되며, {@link VendorId#generateId()} 를 통해 새로운 업체 식별자를 생성한다.
    *
-   * @param request 업체 생성 요청 정보
+   * @param registerRequest 업체 생성 요청 정보
    * @return 생성된 {@link Vendor} 엔티티
    * @throws NullPointerException 필수 입력 값이 누락된 경우
    */
-  public static Vendor register(VendorRegisterRequest request) {
+  public static Vendor register(VendorRegisterRequest registerRequest) {
     Vendor vendor = new Vendor();
 
     vendor.id = VendorId.generateId();
-    vendor.name = Objects.requireNonNull(request.name());
-    vendor.type = request.type();
-    vendor.hubId = HubId.of(request.hubId());
-    vendor.address = Address.of(request.address(), request.detailAddress());
-    vendor.coordinate = Coordinate.of(request.latitude(), request.longitude());
+    vendor.name = Objects.requireNonNull(registerRequest.name());
+    vendor.type = registerRequest.type();
+    vendor.hubId = HubId.of(registerRequest.hubId());
+    vendor.address = Address.of(registerRequest.address(), registerRequest.detailAddress());
+    vendor.coordinate = Coordinate.of(registerRequest.latitude(), registerRequest.longitude());
 
     return vendor;
   }
@@ -121,17 +121,17 @@ public class Vendor extends AbstractAuditEntity {
    *   <li>위치 정보(위도/경도)
    * </ul>
    *
-   * @param request 업체 수정 요청 객체
+   * @param updateRequest 업체 수정 요청 객체
    * @throws NullPointerException 필수 값이 누락된 경우
    */
-  public void updateInfo(VendorUpdateRequest request) {
-    this.name = request.name();
-    this.type = request.type();
-    this.hubId = HubId.of(request.hubId());
-    this.address = Address.of(Objects.requireNonNull(request.address()), request.detailAddress());
+  public void updateInfo(VendorUpdateRequest updateRequest) {
+    this.name = updateRequest.name();
+    this.type = updateRequest.type();
+    this.hubId = HubId.of(updateRequest.hubId());
+    this.address = Address.of(Objects.requireNonNull(updateRequest.address()), updateRequest.detailAddress());
     this.coordinate =
         Coordinate.of(
-            Objects.requireNonNull(request.latitude()),
-            Objects.requireNonNull(request.longitude()));
+            Objects.requireNonNull(updateRequest.latitude()),
+            Objects.requireNonNull(updateRequest.longitude()));
   }
 }
