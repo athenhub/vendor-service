869d944babdeef897ada3dfcaad9ff5d72424276
khs96523 <khs96523@gmail.com>
2025-11-22 15:22:01 +0900

    FEAT: 허브 존재 검증 로직 추가
    See also: #19

--- src/main/java/com/athenhub/vendorservice/vendor/infrastructure/client/HubServiceClient.java
+++ src/main/java/com/athenhub/vendorservice/vendor/infrastructure/client/HubServiceClient.java
@@ -1,5 +1,6 @@
 package com.athenhub.vendorservice.vendor.infrastructure.client;
 
+import com.athenhub.vendorservice.vendor.infrastructure.dto.HubInfo;
 import com.athenhub.vendorservice.vendor.infrastructure.dto.HubManagers;
 import java.util.UUID;
 import org.springframework.cloud.openfeign.FeignClient;
@@ -17,11 +18,20 @@ import org.springframework.web.bind.annotation.PathVariable;
 @FeignClient("hub-service")
 public interface HubServiceClient {
   /**
+   * 지정된 허브의 상세 정보를 조회한다.
+   *
+   * @param hubId 허브 식별자(UUID)
+   * @return 멤버 정보 객체 {@link HubInfo}
+   */
+  @GetMapping("v1/hubs/{hubId}")
+  HubInfo getHubInfo(@PathVariable("hubId") UUID hubId);
+
+  /**
    * 지정된 허브의 관리자 정보를 조회한다.
    *
    * @param hubId 허브 식별자(UUID)
    * @return 허브 관리자 정보 객체 {@link HubManagers}
    */
   @GetMapping("v1/hubs/{hubId}/managers")
-  HubManagers getHubInfo(@PathVariable("hubId") UUID hubId);
+  HubManagers getHubManagers(@PathVariable("hubId") UUID hubId);
 }
